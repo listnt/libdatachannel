a7d9037e5d35564ddc39a05bd8227eabb0ecb818
aidar <aidar1998@gmail.com>
2026-03-09 09:40:42 +0500

    moved files a little

--- include/rtc/jitterbuffer.h
+++ include/rtc/jitterbuffer.h
@@ -5,28 +5,28 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <map>
 
 #include "rtc/rtc.hpp"
 
-class jitterbuffer
-{
-    std::map<std::int16_t, std::vector<std::byte>> _data;
+class jitterbuffer {
+	std::map<std::int16_t, std::vector<std::byte>> _data;
 
-    std::int64_t decoding_started_ts = 0;
+	std::int64_t decoding_started_ts = 0;
 
-    std::uint16_t firstSeqNum = 0;
-    std::uint16_t lenght = 0;
-    bool isFirstPresent = false;
-    bool isLastPresent = false;
-    bool isFormed = false;
+	std::uint16_t firstSeqNum = 0;
+	std::uint16_t lenght = 0;
+	bool isFirstPresent = false;
+	bool isLastPresent = false;
+	bool isFormed = false;
 
 public:
-    jitterbuffer() {};
-    std::vector<std::byte> addVp8Packet(std::vector<std::byte> pkg, std::int16_t prevMarkedPkg);
+	jitterbuffer(){};
+	std::vector<std::byte> addVp8Packet(std::vector<std::byte> pkg, std::int16_t prevMarkedPkg);
 
-    // should be used only if addPacket returned frame, i.e. frame is formed
-    bool isKeyFrame();
-    std::vector<std::uint32_t> getPacketsToNack();
+	// should be used only if addPacket returned frame, i.e. frame is formed
+	bool isKeyFrame();
+	std::vector<std::uint32_t> getPacketsToNack();
 };
 
 #endif // JITTERBUFFER_H
